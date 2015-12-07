3db11e95b9450bf5e070bc3de634b893240f0edf
V1King <zz.vik@rambler.ru>
2015-12-08 03:30:43 +0400

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
+  res.end('Hello World!\n');
 }).listen(2000);
 console.log('Server running at port 2000');
